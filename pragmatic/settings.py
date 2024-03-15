57b7fc54dd7185067c6366fb2c2b1e02ea8c95fd
foxrainswap <slowstar916@naver.com>
2024-03-16 03:18:05 +0900

    Static설정 및 CSS 파일 분리

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -25,9 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env(
     env_file = os.path.join(BASE_DIR, '.env'))
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -130,6 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

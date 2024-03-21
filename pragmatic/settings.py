075bd9cb0ea4614bece3dc22cc1483a8485c568f
foxrainswap <slowstar916@naver.com>
2024-03-21 21:44:45 +0900

    superuser, media관련 URL, ROOT 설정

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -140,6 +140,9 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 

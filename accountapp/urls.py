fe8fe67cf630bfd7816696f958033dfc5c31440e
foxrainswap <slowstar916@naver.com>
2024-03-21 20:01:00 +0900

    DeleteView기반 회원탈퇴 구현

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 from django.contrib.auth.views import LogoutView, LoginView
 
 
@@ -13,7 +13,7 @@ urlpatterns =[
 
     path("detail/<int:pk>", AccountDetailView.as_view(), name="detail"),
     path("update/<int:pk>", AccountUpdateView.as_view(), name="update"),
-
+    path("delete/<int:pk>", AccountDeleteView.as_view(), name = "delete"),
 
     path("create/", AccountCreateView.as_view(), name="create"),
 
